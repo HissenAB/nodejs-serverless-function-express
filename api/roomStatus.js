e1a18338d861e16893ab73561d798b58ca339dde
HissenInstruktionsfilmer <holm.morgan06@gmail.com>
2025-09-23 15:42:36 +0200

    Update roomStatus.js

--- api/roomStatus.js
+++ api/roomStatus.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Hantera preflight request
+  //Hantera preflight request
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
